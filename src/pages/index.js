f0c9da32f72e8bb18f1b827eecde31df4df45e98
myrdinaptaliesen <64499780+myrdinaptaliesen@users.noreply.github.com>
2022-05-12 10:55:07 +0100

    Mise en place de la carte et des données sur la page d'accueil

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,35 @@
 import Head from 'next/head'
 import Link from 'next/link'
 import { useAuth } from '@/hooks/auth'
+import { useState, useEffect } from 'react'
+import axios from '@/lib/axios'
 import ApplicationLogo from '@/components/ApplicationLogo'
 import dynamic from "next/dynamic";
-// import EasyMap from '@/components/EasyMap'
 
-export default function Home() {
+
+const Home = () => {
+
     const { user } = useAuth({ middleware: 'guest' })
     const EasyMap = dynamic(() => import("../components/EasyMap"), {
         ssr: false
-      });
+    });
 
-      
+    const [competitions, setCompetitions] = useState([]);
+
+    useEffect(() => {
+        getGompetitions();
+    }, [])
+
+    const getGompetitions = async () => {
+        await axios.get('/api/competition')
+            .then(res => {
+                // console.log("axios :", res.data);
+                setCompetitions(res.data.data);
+            });
+    }
+
+    
+
 
     return (
         <>
@@ -25,7 +43,7 @@ export default function Home() {
                     <div class="flex w-full md:w-1/2 justify-center md:justify-start text-white font-extrabold">
                         <a class="text-gray-600 no-underline hover:no-underline" href="#">
                             <div class="flex flex-col text-2xl pl-2 sm:h-20 sm:flex-row sm:text-4xl">
-                                <ApplicationLogo />
+                                <ApplicationLogo competitions = {competitions}/>
                                 <h1 class="text-center sm:mt-5">Easy Cyclisme</h1>
                             </div>
                         </a>
@@ -49,19 +67,50 @@ export default function Home() {
 
             <div class="bg-slate-100 h-screen">
                 <div class="container mx-auto pt-24 md:pt-16 px-6">
-                <div id="map">
-                    <EasyMap/>
-                </div>
+                    <div id="map">
+                        <EasyMap competitions={competitions}/>
+                    </div>
+                    <div className="row my-4">
+
+                    </div>
                 </div>
             </div>
+
+            <table class="border-collapse table-auto w-full text-sm">
+                <thead>
+                  <tr>
+                    <th class="border-b dark:border-slate-600 font-medium p-4 pr-8 pt-0 pb-3 text-slate-400 dark:text-slate-200 text-left">Date</th>
+                    <th class="border-b dark:border-slate-600 font-medium p-4 pr-8 pt-0 pb-3 text-slate-400 dark:text-slate-200 text-left">Compétition</th>
+                    <th class="border-b dark:border-slate-600 font-medium p-4 pr-8 pt-0 pb-3 text-slate-400 dark:text-slate-200 text-left">Adresse du départ</th>
+                    <th class="border-b dark:border-slate-600 font-medium p-4 pr-8 pt-0 pb-3 text-slate-400 dark:text-slate-200 text-left">CP</th>
+                    <th class="border-b dark:border-slate-600 font-medium p-4 pr-8 pt-0 pb-3 text-slate-400 dark:text-slate-200 text-left">Ville</th>
+                    <th class="border-b dark:border-slate-600 font-medium p-4 pr-8 pt-0 pb-3 text-slate-400 dark:text-slate-200 text-center">Actions</th>
+                  </tr>
+                </thead>
+                <tbody class="bg-white dark:bg-slate-800">
+                  {competitions.map((competition) => (
+                    <tr>
+                      <td class="border-b border-slate-100 dark:border-slate-700 p-4 text-slate-500 dark:text-slate-400">{competition.date_competition}</td>
+                      <td class="border-b border-slate-100 dark:border-slate-700 p-4 text-slate-500 dark:text-slate-400">{competition.name_competition}</td>
+                      <td class="border-b border-slate-100 dark:border-slate-700 p-4 text-slate-500 dark:text-slate-400">{competition.address_competition}</td>
+                      <td class="border-b border-slate-100 dark:border-slate-700 p-4 text-slate-500 dark:text-slate-400">{competition.postal_code_competition}</td>
+                      <td class="border-b border-slate-100 dark:border-slate-700 p-4 text-slate-500 dark:text-slate-400">{competition.city_competition}</td>
+                      
+
+
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
 
             <footer>
 
 
             </footer>
-            
+
         </>
     )
 }
+export default Home;
 
 
