70fc0b1de438fef6f71a67702b1434e8b4bf15c4
myrdinaptaliesen <64499780+myrdinaptaliesen@users.noreply.github.com>
2022-05-16 10:35:04 +0100

    Ebauche design dashboard

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -6,22 +6,33 @@ const Dashboard = () => {
         <AppLayout
             header={
                 <h2 className="font-semibold text-xl text-gray-800 leading-tight">
-                    Dashboard
+                    Tableau de bord
                 </h2>
             }>
 
             <Head>
-                <title>Laravel - Dashboard</title>
+                <title>Administration Easy Admin</title>
             </Head>
 
             <div className="py-12">
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
                     <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                         <div className="p-6 bg-white border-b border-gray-200">
-                            You're logged in!
+                            Vous êtes bien connecté!
                         </div>
                     </div>
                 </div>
+            </div>
+
+            <h2 className='text-center mb-7'>Gestion de l'application</h2>
+
+            <div className="container flex justify-evenly mx-auto ">
+                <div class="grid-col-3 grid-flow-col gap-8 columns-12 w-max flex justify-evenly">
+                    <div className="columns-12 bg-[url('/competition.jpg')] bg-cover h-96">01</div>
+                    <div className="columns-12 bg-[url('/img/hero-pattern.svg')]">09</div>
+                    <div className="columns-12 bg-[url('/img/hero-pattern.svg')]">09</div>
+                </div>
+
             </div>
         </AppLayout>
     )
